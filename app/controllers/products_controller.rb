18f8682fd63c0da943ce67686f35f112e1edb790
Mustafa Farah <mustafafarah8@hotmail.com>
2025-06-24 01:43:19 +0100

    Styling changes and show/index files modified

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,10 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
