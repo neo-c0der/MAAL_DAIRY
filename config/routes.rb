50b20862d13fc84772ca20eeb2b02dab6783f810
Mustafa Farah <mustafafarah8@hotmail.com>
2025-07-02 21:14:30 +0100

    home page modified - card included

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: "pages#home"
+
   get 'reviews/new'
   get 'reviews/create'
   get 'products/index'
   get 'products/show'
-  devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 
   resources :products, only: [:index, :show] do
     resources :reviews, only: [:new, :create]
   end
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
